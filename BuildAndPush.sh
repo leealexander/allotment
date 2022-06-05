f077dd99222c1bd32fee0608e65986a184943097
Lee Alexander <lee.alexander2@nhs.net>
2022-06-05 18:35:33 +0100

    initial

---
+++ BuildAndPush.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e # stop on error
+docker build    \
+    --build-arg IMAGE_TAG_APPEND="-bullseye-slim-arm32v7" \
+    -t leepaulalexander/allotment:latest \
+    -f ./allotment/Dockerfile \
+    .
+docker push leepaulalexander/allotment:latest
