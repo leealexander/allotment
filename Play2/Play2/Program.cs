e80cb8f8091adee5750b133288d17ced52dba743
Lee Alexander <lee.alexander2@nhs.net>
2022-06-09 10:45:09 +0100

    wip

--- Play2/Play2/Program.cs
+++ Play2/Play2/Program.cs
@@ -3,24 +3,33 @@
 using Allotment;
 using System.Device.Gpio;
 
+Console.WriteLine("Starting...");
 using GpioController controller = new();
 var end = false;
+controller.OpenPin(26, PinMode.Output);
+controller.OpenPin(19, PinMode.Output);
+
 while (!end)
 {
+    Console.WriteLine("Enter command:");
     var command = Console.ReadLine();
-    controller.OpenPin(26, PinMode.Output);
-    controller.OpenPin(19, PinMode.Output);
     switch (command?.ToLower())
     {
         case "open door":
+            Console.WriteLine("opening..");
             controller.Write(26, PinValue.High);
+            Console.WriteLine("done!");
             break;
         case "close door":
+            Console.WriteLine("closing..");
             controller.Write(19, PinValue.High);
+            Console.WriteLine("done!");
             break;
         case "low":
+            Console.WriteLine("all to low....");
             controller.Write(26, PinValue.Low);
             controller.Write(19, PinValue.Low);
+            Console.WriteLine("done!");
             break;
         case "exit":
         case "quit":
@@ -29,10 +38,12 @@ while (!end)
             end = true;
             break;
     }
+    Console.WriteLine("shutting down....");
     controller.Write(26, PinValue.Low);
     controller.Write(19, PinValue.Low);
     controller.ClosePin(26);
     controller.ClosePin(19);
+    Console.WriteLine("Finished!");
 }
 
 //IotFunctions iot = new();
