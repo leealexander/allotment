31b7e265a616ccb2ce43ab999f788a042558d34f
Lee Alexander <lee.alexander2@nhs.net>
2022-06-09 09:42:11 +0100

    w

--- Play2/Play2/Program.cs
+++ Play2/Play2/Program.cs
@@ -1,34 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Allotment;
-using Iot.Device.DHTxx;
 
-Console.WriteLine("Starting temp reader...");
-using var dht = new Dht11(12);
+Console.WriteLine("Starting temp reader, press any key to start...");
+Console.ReadLine();
+
+IotFunctions iot = new();
 while (true)
 {
-    var tempSuccess = dht.TryReadTemperature(out var temperature);
-    var humiditySuccess = dht.TryReadHumidity(out var humidity);
-    var tempText = tempSuccess ? temperature.ToString() : "FAILED";
-    var humidityText = humiditySuccess ? humidity.ToString() : "FAILED";
-
-    IotFunctions iot = new();
-    while (true)
+    Console.WriteLine("trying...");
+    var getIotSuccess = await iot.TryGetTempDetailsAsync(tempDetails =>
     {
-        Console.WriteLine("trying...");
-        var getIotSuccess = await iot.TryGetTempDetailsAsync(tempDetails =>
-        {
-            Console.WriteLine($"Temp={tempDetails.Temperature} Humidity={tempDetails.Humidity}");
-        });
-        if (getIotSuccess)
-        {
-            await iot.OpenDoorsAsync();
-            await iot.CloseDoorsAsync();
-        }
-        else
-        {
-            Console.WriteLine("FAILED");
-        };
-        await Task.Delay(3000);
+        Console.WriteLine($"Temp={tempDetails.Temperature} Humidity={tempDetails.Humidity}");
+    });
+    if (getIotSuccess)
+    {
+        await iot.OpenDoorsAsync();
+        await iot.CloseDoorsAsync();
     }
+    else
+    {
+        Console.WriteLine("FAILED");
+    };
+    await Task.Delay(3000);
 }
+
