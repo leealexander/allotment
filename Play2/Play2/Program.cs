13d27eff525ab9a29dfed50000b6a0e6a8deafc6
Lee Alexander <lee.alexander2@nhs.net>
2022-06-09 06:58:22 +0100

    wip

--- Play2/Play2/Program.cs
+++ Play2/Play2/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Press any key to stop");
-Console.ReadKey();
-Console.WriteLine("Stopped");
+using Iot.Device.DHTxx;
+
+Console.WriteLine("Starting temp reader...");
+using var dht = new Dht11(32);
+while (true)
+{
+    var tempSuccess = dht.TryReadTemperature(out var temperature);
+    var humiditySuccess = dht.TryReadHumidity(out var humidity);
+    var tempText = tempSuccess ? temperature.ToString() : "FAILED";
+    var humidityText = humiditySuccess ? tempSuccess.ToString() : "FAILED";
+
+    Console.WriteLine($"{tempText} - {humidityText}");
+    await Task.Delay(1000);
+}
