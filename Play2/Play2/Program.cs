50bb0f73a68d937404e6b484ff194e4ab2fbc819
Lee Alexander <lee.alexander2@nhs.net>
2022-06-09 06:33:30 +0100

    wip

--- Play2/Play2/Program.cs
+++ Play2/Play2/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Press any key to stop");
+Console.ReadKey();
+Console.WriteLine("Stopped");
