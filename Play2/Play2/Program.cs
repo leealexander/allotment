07e0f6e8ebbaf1d9c42f223c87f8ed12f1f84fd0
Lee Alexander <lee.alexander2@nhs.net>
2022-06-09 07:02:48 +0100

    wip

--- Play2/Play2/Program.cs
+++ Play2/Play2/Program.cs
@@ -8,7 +8,7 @@ while (true)
     var tempSuccess = dht.TryReadTemperature(out var temperature);
     var humiditySuccess = dht.TryReadHumidity(out var humidity);
     var tempText = tempSuccess ? temperature.ToString() : "FAILED";
-    var humidityText = humiditySuccess ? tempSuccess.ToString() : "FAILED";
+    var humidityText = humiditySuccess ? humidity.ToString() : "FAILED";
 
     Console.WriteLine($"{tempText} - {humidityText}");
     await Task.Delay(1000);
