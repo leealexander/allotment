883e043b4c08cfd42bb5e6338c0dd27f08debb0e
Lee Alexander <lee@binarycomponents.com>
2023-11-26 09:50:35 +0000

    Potential workaround fix and more logging info..

--- allotment/Machine/Monitoring/WaterLevelMonitor.cs
+++ allotment/Machine/Monitoring/WaterLevelMonitor.cs
@@ -19,28 +19,35 @@ namespace Allotment.Machine.Monitoring
 
         public async Task RunAsync(IRunContext ctx)
         {
-            //  We don't want to measure when water is being drawn as this could effect the reading
-            await _auditLogger.AuditLogAsync("Checking water level...");
-            if (_machineService.IsWaterOn)
+            try
             {
-                await _auditLogger.AuditLogAsync("Water is already on so waiting 30secs...");
-                ctx.RunAgainIn(TimeSpan.FromSeconds(30));
-                return;
-            }
+                //  We don't want to measure when water is being drawn as this could effect the reading
+                await _auditLogger.AuditLogAsync("Checking water level...");
+                if (_machineService.IsWaterOn)
+                {
+                    await _auditLogger.AuditLogAsync("Water is already on so waiting 30secs...");
+                    ctx.RunAgainIn(TimeSpan.FromSeconds(30));
+                    return;
+                }
 
-            if (!_machineService.IsWaterLevelSensorOn)
-            {
-                await _auditLogger.AuditLogAsync("Turning on water monitor..");
-                await _machineService.WaterLevelMonitorOnAsync();
+                if (!_machineService.IsWaterLevelSensorOn)
+                {
+                    await _auditLogger.AuditLogAsync("Turning on water monitor..");
+                    await _machineService.WaterLevelMonitorOnAsync();
+                }
+                else
+                {
+                    await _auditLogger.AuditLogAsync("Water monitor already on, doing nothing!");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                await _auditLogger.AuditLogAsync("Water monitor already on, doing nothing!");
+                await _auditLogger.AuditLogAsync($"Exception in water logger: {ex.Message}");
             }
 
             var sleepDuration = (await _settingsStore.GetAsync()).Irrigation.WaterLevelSensor.PeriodicCheckDuration;
-            await _auditLogger.AuditLogAsync($"Sleeping for {sleepDuration}");
             ctx.RunAgainIn(sleepDuration);
+            await _auditLogger.AuditLogAsync($"Sleeping for {sleepDuration}");
         }
     }
 }
